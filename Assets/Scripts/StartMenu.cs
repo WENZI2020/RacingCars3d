7826ec54ea88510c01dcb4227de57206e20c87fc
曾昭文 <wenzi2020@yeah.net>
2020-07-12 02:25:15 +0800

    The project has done to publish at an early stage.

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum GAMETYPE
+{
+    SINGLE,
+    MULTI
+}
+public class StartMenu : MonoBehaviour
+{
+    public GameObject panelA;
+    public GameObject panelB;
+    public GAMETYPE gAMETYPE;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelA.SetActive(true);
+        panelB.SetActive(false);
+    }
+    void Update()
+    {
+
+    }
+    public void PanelA()
+    {
+        panelA.SetActive(true);
+        panelB.SetActive(false);
+    }
+    public void PanelB()
+    {
+        panelA.SetActive(false);
+        panelB.SetActive(true);
+    }
+    // Update is called once per frame
+    public void PlaySingle()
+    {
+        gAMETYPE = GAMETYPE.SINGLE;
+        PlayerPrefs.SetString("GAMETYPE", "SINGLE");
+        SceneManager.LoadScene(1);
+    }
+    public void PlayMulti()
+    {
+        gAMETYPE = GAMETYPE.MULTI;
+        PlayerPrefs.SetString("GAMETYPE", "MULTI");
+        SceneManager.LoadScene(1);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Standard Assets/Vehicles/Car/Scripts/SmoothFlow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFlow : MonoBehaviour
+{
+    private Transform target;
+    private float height = 3;
+    private float distance = -6;
+    private float smoothSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Car").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 meForward = this.transform.forward;
+        Vector3 targetTranForward = target.forward;
+        meForward.y = 0;
+        targetTranForward.y = 0;
+        Vector3 forward = Vector3.Lerp(meForward.normalized, targetTranForward.normalized, smoothSpeed * Time.deltaTime);
+        this.transform.position = forward * distance + Vector3.up * height + target.position;
+        this.transform.LookAt(target);
+    }
+}
